f0a8baf6e5cc6c400cef02010cabeb88728f9fbb
diglean <diegoleandro2002@gmail.com>
2022-06-02 14:41:06 -0300

    Context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './contexts/auth';
 import Routes from './routes';
 
 function App() {
   return (
-    <div >
+    <AuthProvider>
       <BrowserRouter>
-      <Routes/>
+        <Routes/>
       </BrowserRouter>
-    </div>
+    </AuthProvider>
+    
   );
 }
 
